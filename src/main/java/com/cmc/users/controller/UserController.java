9ba377b8d831516ba5836cfa920e5d3d78d52d99
Rajesh Bhoopathy Raju <rajesh.raju@moogsoft.com>
2021-09-05 23:52:23 -0700

    Update controller

--- src/main/java/com/cmc/users/controller/UserController.java
+++ src/main/java/com/cmc/users/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.cmc.users.controller;
 
+import com.cmc.users.controller.model.Signin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +15,24 @@ public class UserController {
     }
 
 
+    @PostMapping(path = "/signin", consumes = "application/json", produces = "application/json")
+    public String login(@RequestBody Signin signin) {
+        String status = validate(signin.getUsername(), signin.getPassword()) ? "success" : "error";
+
+
+        return "{ " +
+                "\"username\": \"" + signin.getUsername() + "\"," +
+                "\"status\": \"success\"" +
+                " }";
+    }
+
+    private boolean validate(String username, String password) {
+        boolean isValid = false;
+
+        if (username.equalsIgnoreCase("admin") || username.equalsIgnoreCase("root"))
+            if (password.equals("password"))
+                isValid = true;
+
+        return isValid;
+    }
 }
